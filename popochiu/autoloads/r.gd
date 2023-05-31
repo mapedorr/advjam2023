aa0763834ccd787e0cde85f5db7e815dc66fc0d4
carenalga <mapedorr@gmail.com>
2023-05-30 23:56:15 -0500

    upd: added concept image and some shaders to test shaders

--- popochiu/autoloads/r.gd
+++ popochiu/autoloads/r.gd
@@ -2,11 +2,14 @@
 extends "res://addons/popochiu/engine/interfaces/i_room.gd"
 
 # classes ----
+const PRTests := preload('res://popochiu/rooms/tests/room_tests.gd')
 # ---- classes
 
 # nodes ----
+var Tests: PRTests : get = get_Tests
 # ---- nodes
 
 # functions ----
+func get_Tests() -> PRTests: return super.get_runtime_room('Tests')
 # ---- functions
 
